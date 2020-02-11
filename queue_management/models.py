6b02b6d9374e97f053d670bc0f1cfa44e8a18a3e
Sabrina Gelbart <sabrina.gelbart@gmail.com>
2020-02-10 19:19:33 -0600

    initial commit (models working)

---
+++ queue_management/models.py
@@ -0,0 +1,22 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Queue(models.Model):
+    name = models.CharField(max_length=200)
+
+class Member(models.Model):models
+    full_name = models.CharField(max_length=200)
+    pin = models.IntegerField()
+
+class QueueMember(models.Model):
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    queue = models.ForeignKey(Queue, on_delete=models.CASCADE)
+    added_at = models.DateTimeField(default=timezone.now)
+    outcome_id = models.ForeignKey('Outcome', on_delete=models.SET_NULL, null=True)
+    outcome_notes = models.TextField()
+    resolved_at = models.DateTimeField(default=timezone.now)
+
+class Outcome(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
